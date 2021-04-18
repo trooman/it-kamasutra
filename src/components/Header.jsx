a970d8bed8376355752ba34cad1f924e37f47048
unknown <you@example.com>
2021-04-18 14:28:28 +0300

    add jsx modules

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <header className='header'>
+        <img
+            src='https://www.flaticon.com/svg/vstatic/svg/40/40021.svg?token=exp=1618632762~hmac=0ab16a6ede2e1846ff9f05687d1ac289'></img>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img
+                    src='https://www.abahanavillas.com/documents/20182/48060/Playa+Poniente.jpg/5b08de30-b8cb-48d1-9cea-164bde7fd2a2?version=1.1&t=1548152598721&download=true'></img>
+            </div>
+            <div>
+                avatar + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
